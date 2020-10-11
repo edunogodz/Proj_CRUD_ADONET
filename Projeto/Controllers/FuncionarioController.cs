e0eb68ab98a177b7b9c5f5d1a5ec120132806a38
carlos <edunogodz@gmail.com>
2020-10-11 18:16:57 -0300

    Add project files.

---
+++ Projeto/Controllers/FuncionarioController.cs
@@ -0,0 +1,94 @@
+﻿using Projeto.Models;
+using Projeto.Repository;
+using System.Web.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario/SelecionarFuncionarios
+        public ActionResult SelecionarFuncionarios()
+        {
+            var funcRepository = new FuncionarioRepository();
+            ModelState.Clear();
+
+            return View(funcRepository.SelecionarFuncionarios());
+        }
+
+        //  GET: Funcionario/AdicionarFuncionario
+        public ActionResult AdicionarFuncionario()
+        {
+            return View();
+        }
+
+        //  POST: Funcionario/AdicionarFuncionario
+        [HttpPost]
+        public ActionResult AdicionarFuncionario(Funcionario func)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var funcRepository = new FuncionarioRepository();
+
+                    if (funcRepository.AdicionarFuncionario(func))
+                    {
+                        ViewBag.Message = "Funcionário criado com sucesso!";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Funcionario/AtualizarFuncionario/5
+        public ActionResult AtualizarFuncionario(int id)
+        {
+            var funcRepository = new FuncionarioRepository();
+
+            return View(funcRepository.SelecionarFuncionarios()
+                            .Find(func => func.IdFuncionario == id));
+        }
+
+        // POST: Funcionario/Edit/5
+        [HttpPost]
+        public ActionResult AtualizarFuncionario(int id, Funcionario funcionario)
+        {
+            try
+            {
+                var funcRepository = new FuncionarioRepository();
+                funcRepository.AtualizarFuncionario(funcionario);
+
+                return RedirectToAction("SelecionarFuncionarios");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Funcionario/ExcluirFuncionario/5
+        public ActionResult ExcluirFuncionario(int id)
+        {
+            try
+            {
+                var funcRepository = new FuncionarioRepository();
+
+                if (funcRepository.ExcluirFuncionario(id))
+                {
+                    ViewBag.AlertMsg = "Funcionário excluído com sucesso.";
+                }
+
+                return RedirectToAction("SelecionarFuncionarios");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Projeto/Repository/Conexao.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace Projeto.Repository
+{
+    public class Conexao
+    {
+        protected SqlConnection Con;
+
+        protected SqlCommand Cmd;
+
+        protected SqlDataReader Dr;
+
+        //Método para abrir a conexão com o SqlServer:
+        protected void AbrirConexao()
+        {
+            try
+            {
+                Con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookListRazor;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+                //< add name = "DatabaseCrud" connectionString = "Data Source=(localdb)MSSQLLocalDB; Initial Catalog=BookListRazor; Integrated Security=True; MultipleActiveResultSets=True;"
+                //providerName = "System.Data.SqlClient" />
+                //< add name = "DefaultConnection" connectionString = "Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=BookListRazor; Integrated Security=True; MultipleActiveResultSets=True;" />   
+                //< add name = "PodioAspnetSampleDb" connectionString = "server=(localdb)\MSSQLLocalDB;database=BookListRazor; Integrated Security=True; MultipleActiveResultSets=True" providerName = "System.Data.SqlClient" />
+
+                //Outra maneira de criar uma connection string (usando o Web.Config
+                //Con = new SqlConnection(WebConfigurationManager.ConnectionStrings["ProjetoCRUD"].ConnectionString);
+                Con.Open();
+            }
+            catch (Exception e)
+            {
+                // Caso dê algum problema, enviar uma mensagem informando o erro:
+                throw new Exception("Erro ao abrir a conexão: " + e.Message);
+            }
+        }
+
+        //Método para fechar a conexão:
+        protected void FecharConexao()
+        {
+            try
+            {
+                if (Con != null)
+                {
+                    Con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                // Caso dê algum problema, enviar uma mensagem informando o erro:
+                throw new Exception("Erro ao fechar a conexão: " + e.Message);
+            }
+        }
+    }
+}
